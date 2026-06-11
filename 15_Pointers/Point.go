487dc34028b137b8a396be1b6ee5ee73487f3c2f
Naveen <rajnaveen13.av@gmail.com>
2026-06-11 17:50:13 +0530

    Closures Basic

---
+++ 15_Pointers/Point.go
@@ -0,0 +1,39 @@
+// A pointer stores the memory address of another variable
+/*Pointers are useful when you want to:
+->modify a variable inside a function
+->avoid copying large data structures
+->share data efficiently
+//Symbols:
+ ->  &x        -     address of x
+ ->  *p        -     value stored at pointer p
+ ->  var p *int  -   pointer to int
+
+*/
+
+package main
+
+import "fmt"
+
+//by value
+//func changeNum(num int) {
+//num = 5
+//fmt.Println("In changeNum", num)
+//}
+
+//by refrence
+
+func changeNum(num *int) {
+	*num = 5
+	fmt.Println("In changeNum", *num)
+}
+
+func main() {
+	num := 1
+	//changeNum(num)
+	changeNum(&num)
+
+	// for memeory address
+	//fmt.Println("Memory address", &num)
+	fmt.Println("After changeNum is main", num)
+
+}
