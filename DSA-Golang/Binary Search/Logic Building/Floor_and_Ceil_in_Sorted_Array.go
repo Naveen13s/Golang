7473a6ace4f4d9cd2aa8937173927fabd39ef8aa
Naveen <rajnaveen13.av@gmail.com>
2026-09-04 19:28:14 +0530

    Add Floor and Ceil in Sorted Array Solution in Go

--- DSA-Golang/Binary Search/Logic Building/Floor_and_Ceil_in_Sorted_Array.go
+++ DSA-Golang/Binary Search/Logic Building/Floor_and_Ceil_in_Sorted_Array.go
@@ -4,21 +4,15 @@
 func getFloorAndCeil(nums []int, x int) []int {
 	floor := -1
 	ceil := -1
-
 	low, high := 0, len(nums)-1
-
 	for low <= high {
 		mid := low + (high-low)/2
-
 		if nums[mid] == x {
-			// Exact match is both the floor and the ceil
 			return []int{nums[mid], nums[mid]}
 		} else if nums[mid] < x {
-			// Candidate for floor, look right for a larger possible floor
 			floor = nums[mid]
 			low = mid + 1
 		} else {
-			// Candidate for ceil, look left for a smaller possible ceil
 			ceil = nums[mid]
 			high = mid - 1
 		}
